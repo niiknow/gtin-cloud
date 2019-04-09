055d40b115dffb9b08be49b6ed8d7111e4ff7fa9
noogen <tom@noogen.net>
2019-04-09 15:56:10 -0500

    convert all to async

--- src/storeHandler.js
+++ src/storeHandler.js
@@ -15,43 +15,54 @@ const debug = require('debug')('gtin-cloud')
  */
 export default async (event, context, callback) => {
   // Parameters
-  //  @param  string     gtin    the gtin
+  //  @param  string     gtin         the gtin
   //  @param  string     image_url    the image url
-  //  @param  string     data_url     the data url
+  //  @param  string     type         image or media
   //  @param  string     vendor       the vendor (optional - default empty)
-  const gtin       = event.pathParameters.gtin
+  const gtin       = `00000000000000${event.pathParameters.gtin}`.slice(-14)
   const qs         = event.queryStringParameters || {}
-  const url        = qs.url
-  const type       = qs.type || ''
+  const url        = (qs.url || '').trim()
+  const type       = (qs.type || '').trim()
   const rspHandler = res(context, callback)
   const rawUrl     = url.split(/\#|\?/)[0];
   const fileName   = (qs.name || rawUrl.split('/').pop()).trim().toLowerCase();
   const ext        = fileName.split('.').pop();
   const basePath   = gtinPath(gtin, event.pathParameters.vendor);
   const body       = (event.body || '').trim()
-  let destPath     = basePath + 'index.jpg'
+  const tasks      = []
+  let destPath     = ''
 
-  // possible action values: image or media
-  // image is featured image
+  // validate parameters
+  if (type.length > 0 && url.length <= 0) {
+    return rspHandler(`url querystring parameter is required for type of ${type}`, 422)
+  }
+
+  // handle image
   if (type.indexOf('image') > -1) {
     if (ext !== 'jpg' || ext !== 'jpeg') {
       return rspHandler(`${fileName} extension must be jpg/jpeg`, 422)
     }
 
     // download and store image as index.jpg
+    destPath = basePath + 'index.jpg'
     const fstream = got.stream(url)
-    await saveToS3(destPath, fstream, 'image/jpeg');
+    tasks.push(saveToS3(destPath, fstream, 'image/jpeg'));
   } else if (type.indexOf('media') > -1) {
-    // handle media storage
-    fstream  = got.stream(url)
+    // handle media
     destPath = basePath + 'media/' + fileName
-    await saveToS3(destPath);
+    fstream  = got.stream(url)
+    tasks.push(saveToS3(destPath));
+  } else if (type.length > 0) {
+    return rspHandler(`Unknown type ${type}`, 422)
   }
 
   // only store if body is a json
   if (body.indexOf('{') > 0) {
-    await saveToS3(basePath + 'index.json', event.body, 'application/json');
+    tasks.push(saveToS3(basePath + 'index.json', event.body, 'application/json'));
   }
+
+  // process all async tasks
+  await Promise.all(tasks);
 
   return rspHandler(`Uploaded as: ${destPath}`, 422)
 }
