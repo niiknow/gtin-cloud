be3e461ec44849597c034798e91f746363e83f68
noogen <tom@noogen.net>
2019-04-09 17:46:37 -0500

    add ItemMaster api

--- src/researchHandler.js
+++ src/researchHandler.js
@@ -1,13 +1,14 @@
 import res      from './response'
 import got      from 'got'
+import xmljs    from 'xml-js'
 
 const debug = require('debug')('gtin-cloud')
 
 class Handlers {
-  static async eanDataRequest(ean13) {
+  static async eanDataRequest(gtin) {
     const query = {
       v: 3,
-      find: `0000000000000${ean13}`.trim().slice(-13),
+      find: `0000000000000${gtin}`.slice(-13),
       keycode: 'CDED97825A77DBEB',
       mode: 'json'
     }
@@ -17,7 +18,38 @@ class Handlers {
     return rst.body
   }
 
-  static async itemMasterRequest(ean13) {
+  static async itemMasterRequest(gtin) {
+    gtin = `0000000000000${gtin}`.slice(-14)
+
+    const url = `https://api.itemmaster.com/v2.2/item/`
+
+    const headers = {
+      username: process.env.IM_USER,
+      password: process.env.IM_PASS
+    }
+
+    const query = {
+      upc: gtin,
+      ef: 'jpg',
+      eip: 75,
+      epf: 1000,
+      allImg: 'Y'
+    }
+
+    const rst  = await got(url, { query, headers })
+    const json = xmljs.xml2json(rst.body, {
+      compact: true,
+      ignoreDeclaration: true
+    })
+    const obj = JSON.parse(json)
+    const itemJson = JSON.stringify(obj.items.item)
+    // console.log(JSON.stringify(obj.items.item, null, 2))
+    debug(itemJson)
+    return itemJson
+  }
+
+  static async kwikeeRequest(ean13) {
+
   }
 }
 
@@ -34,17 +66,20 @@ export default async (event, context, callback) => {
   const qs         = event.queryStringParameters || {}
   const vendor     = (event.pathParameters.vendor || '').toLowerCase()
   const rspHandler = res(context, callback)
-  const ean13      = (qs.q || '').trim()
+  const gtin       = (qs.q || '').trim()
 
-  if (ean13.length < 13) {
-    return rspHandler(`${ean13} must be at least 13 characters`, 422)
+  if (gtin.length < 14) {
+    return rspHandler(`${ean13} must be at least 14 characters`, 422)
   }
 
   if (vendor === 'eandata') {
-    const rst = await Handlers.eanDataRequest(ean13)
+    const rst = await Handlers.eanDataRequest(gtin)
     return rspHandler(rst)
   } else if (vendor === 'itemmaster') {
-    const rst = await Handlers.itemMasterRequest(ean13)
+    const rst = await Handlers.itemMasterRequest(gtin)
+    return rspHandler(rst)
+  } else if (vendor === 'kwikee') {
+    const rst = await Handlers.kwikeeRequest(gtin)
     return rspHandler(rst)
   }
 
--- tests/researchHandler.test.js
+++ tests/researchHandler.test.js
@@ -1,6 +1,11 @@
 import { handlers } from '../src/researchHandler'
 
-test('successful ean lookup 3045320094084', async () => {
-  const rst = await handlers.handleEanData('3045320094084')
+/*test('successful eandata lookup 3045320094084', async () => {
+  const rst = await handlers.eanDataRequest('3045320094084')
+  expect(rst).not.toBeNull()
+});*/
+
+test('successful itemmaster lookup 00832544000501', async () => {
+  const rst = await handlers.itemMasterRequest('00832544000501')
   expect(rst).not.toBeNull()
 });
