995878c229923deab17445bf8134135313e2dab0
noogen <tom@noogen.net>
2019-04-10 16:34:36 -0500

    update test message

--- tests/researchHandler.test.js
+++ tests/researchHandler.test.js
@@ -2,19 +2,19 @@ import { handlers } from '../src/researchHandler'
 
 jest.setTimeout(30000)
 
-test('successful eandata lookup 03045320094084', async () => {
+test('eandata lookup 03045320094084', async () => {
   const rst = await handlers.eanDataRequest('03045320094084', false)
   // console.log(rst)
   expect(rst).not.toBeNull()
 });
 
-test('successful itemmaster lookup 00832544000501', async () => {
+test('itemmaster lookup 00832544000501', async () => {
   const rst = await handlers.itemMasterRequest('00832544000501', false)
   // console.log(rst)
   expect(rst).not.toBeNull()
 });
 
-test('successful itemmaster lookup 00018200281372', async () => {
+test('kwikee lookup 00018200281372', async () => {
   const rst = await handlers.kwikeeRequest('00018200281372', false)
   // console.log(rst)
   expect(rst).not.toBeNull()
