d69bc78691ac5d24314a800063689bcbf28786ac
noogen <tom@noogen.net>
2019-04-21 12:22:00 -0500

    add digiteyes log

---
+++ tests/secondaryVendors.test.js
@@ -0,0 +1,15 @@
+import handlers from '../src/secondaryVendors'
+
+jest.setTimeout(30000)
+
+describe('digiteyes-research-tests', () => {
+  test('digiteyes lookup 00078732004245', async () => {
+    const rst = await handlers.digiteyesRequest('00078732004245', false)
+    // console.log(rst)
+    expect(rst).not.toBeNull()
+    expect(rst.gtin).toBe('00078732004245')
+    expect(rst.image).not.toBeNull()
+    expect(rst.gtin_path).not.toBeNull()
+    expect(rst._ts).not.toBeNull()
+  })
+})
