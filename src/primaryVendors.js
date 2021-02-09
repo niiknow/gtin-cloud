09d7928deede8f9087eb2aaeeb9d75c1fe27d2dc
noogen <tom@noogen.net>
2021-02-09 12:49:51 -0600

    update package, fallback on ecommerce image

--- src/primaryVendors.js
+++ src/primaryVendors.js
@@ -122,6 +122,89 @@ class Handlers {
           let asset = Handlers.syndigoExtractAsset(components, 'e3238a4c-1936-400d-84c0-63cb320c24ce')
           if (asset) {
             image = `https://assets.edgenet.com/${asset.Value}?filetype=jpg&size=1000&numratio=1&denratio=1&crop=False`
+          } else {
+            asset = Handlers.syndigoExtractAsset(components, '7cf656a0-1f74-4ed0-bb06-7de031f6e05c')
+            if (asset) {
+              image = `https://assets.edgenet.com/${asset.Value}?filetype=jpg&size=1000&numratio=1&denratio=1&crop=False`
+            }
+          }
+        }
+
+        product.gtin      = myGtin
+        product.image     = image
+        product.gtin_path = gtinPath(myGtin)
+        product._ts       = (new Date()).toISOString()
+
+        if (storeData) {
+          // stash the data and image
+          const rsp = storeTasks(myGtin, image, 'image', product, 'syndigo', null, { headers })
+          debug('prep to store data', rsp.tasks.length)
+          if (rsp.tasks.length > 0) {
+            debug('storing data')
+            await Promise.all(rsp.tasks)
+          }
+        }
+      }
+
+      return product
+    } catch(e) {
+      syndigoAuthValue = ''
+      debug(JSON.stringify(e, null, 2))
+      return { error: 'request error', ex: e }
+    }
+  }
+
+  static async syndigoLastModified(moDate = new Date()) {
+    const baseUrl  = process.env.SYNDIGO_URL
+    const userName = encodeURIComponent(process.env.SYNDIGO_USERNAME)
+    const secret   = encodeURIComponent(process.env.SYNDIGO_SECRET)
+    const ownerId  = process.env.SYNDIGO_DATAOWNERID
+
+    if (!syndigoAuthValue) {
+      // auth
+      const authResult = await got(`${baseUrl}/api/auth?username=${userName}&secret=${secret}`, { responseType: 'json' })
+      // get Value
+      syndigoAuthValue = authResult.body.Value
+    }
+
+    const url = `${baseUrl}/ui/product/`
+    const headers = {
+      'Authorization': `EN ${syndigoAuthValue}`
+    }
+    const json = {
+      'OrderBy': '0994d0f8-35e7-4a6d-9cd9-2ae97cd8b993',
+      'Desc': false,
+      "DateFilters":[
+        {
+          "Name": "LastModifiedDate",
+          "Operator": "GreaterThan",
+          "Value": moDate.toISOString(),
+          "IncludeMissing":true
+        }
+      ],
+      'AttributeFilterOperator': 'Or',
+      'Archived': false,
+      'OnHold': false,
+      'DataOwner': ownerId
+    }
+    const searchParams = {
+      skip: 0,
+      take: 1
+    }
+    let myGtin = `0000000000000${gtin}`.slice(-14)
+
+    try {
+      const rst = await got.post(url, { json, headers, searchParams, responseType: 'json' })
+      let product   = rst.body.Results[0]
+      let image     = imageUrl
+
+      if (product && product.Components && image == null) {
+        // console.log(JSON.stringify(obj.items.item, null, 2))
+        const components = product.Components
+        if (Array.isArray(components)) {
+          let asset = Handlers.syndigoExtractAsset(components, 'e3238a4c-1936-400d-84c0-63cb320c24ce')
+          if (asset) {
+            image = `https://assets.edgenet.com/${asset.Value}?filetype=jpg&size=1000&numratio=1&denratio=1&crop=False`
           }
         }
 
